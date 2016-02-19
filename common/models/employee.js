34e09b261bc140c3d459393170f9c12333abfb78
Jamal Osman <JamalOsman@Cohaesuss-MacBook-Pro-8.local>
2016-02-19 11:39:09 +0000

    Commit build products

--- common/models/employee.js
+++ common/models/employee.js
@@ -1,3 +1,16 @@
-module.exports = function(Employee) {
+module.exports = function (Employee) {
+    Employee.create = function (data, cb) {
+        console.log("OVERWRITTEN CREATE METHOD");
 
+        data.image = this.storeImage();
+        console.log(data);
+        create.call(Employee, data, cb)
+    }
+
+
+    Employee.storeImage = function (image) {
+        // TODO: parse uploaded image, save in file system and return the relative URL
+
+        return null;
+    }
 };
